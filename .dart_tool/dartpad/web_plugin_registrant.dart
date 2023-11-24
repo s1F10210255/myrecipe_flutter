ada49b04bff3067e5f573d749e3b6f7f63ed6ba1
s1F10210255 <s1f102102559@iniad.org>
2023-11-24 09:25:09 +0900

    fetch2

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,34 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:rive_common/rive_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:youtube_player_iframe_web/src/web_youtube_player_iframe_platform.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  RivePlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WebYoutubePlayerIframePlatform.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
---
+++ lib/views/photos_page.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hd_splash_flutter/app/extensions/extensions.dart';
+import 'package:hd_splash_flutter/core/type/enum.dart';
+import 'package:unsplash_dart/unsplash_dart.dart';
+
+import '../photos.dart';
+import '../../components/components.dart';
+
+class PhotosPage extends StatelessWidget {
+  const PhotosPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // final data = context.read<Unsplash>().clientRateLimit;
+    // log(data.toString());
+    return CustomNestedScrollHead(
+        title: context.lang('photos'),
+        subtitle: context.lang("sub-photos"),
+        sortChild: BlocBuilder<PhotosCubit, PhotosState>(
+          buildWhen: (previous, current) => previous.orderBy != current.orderBy,
+          builder: (context, state) {
+            return ButtonSortOrderBy(
+              listValue: PhotosOrderBy.values,
+              selected: state.orderBy,
+              onSelected: (orderBy) =>
+                  context.read<PhotosCubit>().changeOrderBy(orderBy),
+            );
+          },
+        ),
+        body: AppInternetWidget(
+          reload: (reconnectInternet) {
+            if (reconnectInternet) {
+              context.read<PhotosCubit>().loadingPhotos();
+            }
+          },
+          child: BlocBuilder<PhotosCubit, PhotosState>(
+              buildWhen: (previous, current) =>
+                  previous.status != current.status,
+              builder: ((context, state) {
+                switch (state.status) {
+                  case StatusType.loading:
+                    return const AppLoadingWidget();
+                  case StatusType.error:
+                    return const AppErrorWidget();
+                  case StatusType.loaded:
+                    return const PhotosGridView();
+                  default:
+                    return const SizedBox();
+                }
+              })),
+        ));
+  }
+}
